127354338e53bafca02065ad11cd9b6aee6f3a9a
LucasFriis <70882484+LucasFriis@users.noreply.github.com>
2023-03-13 15:52:38 +0100

    added extern api request and response

---
+++ src/components/ApiResult.vue
@@ -0,0 +1,44 @@
+<template>
+<div v-for="result in apiResults" class="api-result">
+    <div>
+        <p>ID: {{result.id}}</p>
+        <h3>{{result.title}}</h3>
+        <p>Completed: {{result.completed}}</p>
+    </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'ApiResult',
+    data() {
+      return {
+        apiResults: []
+      }
+    },
+    methods: {
+      async getData() {
+        const apiResponse = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const res = await apiResponse.json();
+        this.apiResults = res;
+      }
+    },
+    mounted() {
+        this.getData();
+    }
+  }
+</script>
+
+<style>
+
+.api-result {
+    background-color: rgba(0, 162, 255, 0.514);
+    width: 300px;
+    border-radius: 10px;
+    padding: 16px;
+    margin: 40px auto;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+}
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img style="height: 100px;" alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Resultat fra ekstern API"/>
+    <ApiResult />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import ApiResult from '@/components/ApiResult.vue'; // @ is an alias to /src
 
 @Options({
   components: {
     HelloWorld,
+    ApiResult
   },
 })
 export default class HomeView extends Vue {}
